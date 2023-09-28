00a9d32533420a866b7d2762d576e419d006121f
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2023-09-28 05:04:03 +0000

    feat(ex1): filenames as command-line args

--- ex1.py
+++ ex1.py
@@ -1,5 +1,6 @@
 """Ex1.py - PROCESAMIENTO DE SECUENCIAS"""
 
+import argparse
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 
@@ -7,21 +8,29 @@ def translate_sequence(dna_sequence):
     """Función para traducir una secuencia de nucleótidos a una secuencia de aminoácidos."""
     return dna_sequence.translate()
 
-INPUT_GB_FILE = "input/NM_000311-5.gb"
-OUTPUT_FAS_FILE = "output/output_NM_000311-5.fas"
+def main(input_genbank_file, output_fasta_file):
+    """Traduce features CDS de un archivo GenBank a una secuencia de aminoácidos en formato FASTA."""
+    translated_sequences = []
+    with open(input_genbank_file, "r", encoding="utf8") as genbank_handle:
+        for record in SeqIO.parse(genbank_handle, "genbank"):
+            for feature in record.features:
+                if feature.type == "CDS":
+                    nucleotide_sequence = feature.location.extract(record).seq      # obtengo secuencia de nucleótidos indicada por el record
+                    amino_acid_sequence = translate_sequence(nucleotide_sequence)
+                    fasta_record = SeqRecord(amino_acid_sequence, id=feature.qualifiers.get("gene")[0])     # creo un registro FASTA con la secuencia traducida
+                    translated_sequences.append(fasta_record)
+    with open(output_fasta_file, "w", encoding="utf8") as fasta_handle:
+        SeqIO.write(translated_sequences, fasta_handle, "fasta")    # escribo la(s) secuencia(s) traducida(s) en un archivo FASTA
+    print(f"{len(translated_sequences)} secuencia{'s' if len(translated_sequences) > 1  else ''} en {output_fasta_file}")
 
-translated_sequences = []
-
-with open(INPUT_GB_FILE, "r", encoding="utf8") as genbank_handle:
-    for record in SeqIO.parse(genbank_handle, "genbank"):
-        for feature in record.features:
-            if feature.type == "CDS":
-                nucleotide_sequence = feature.location.extract(record).seq      # obtengo secuencia de nucleótidos indicada por el record
-                amino_acid_sequence = translate_sequence(nucleotide_sequence)
-                fasta_record = SeqRecord(amino_acid_sequence, id=feature.qualifiers.get("gene")[0])     # creo un registro FASTA con la secuencia traducida
-                translated_sequences.append(fasta_record)
-
-with open(OUTPUT_FAS_FILE, "w", encoding="utf8") as fasta_handle:
-    SeqIO.write(translated_sequences, fasta_handle, "fasta")    # escribo la(s) secuencia(s) traducida(s) en un archivo FASTA
-
-print(f"{len(translated_sequences)} secuencia{'s' if len(translated_sequences) > 1  else ''} en {OUTPUT_FAS_FILE}")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Translate CDS features from GenBank file to FASTA amino acid sequences.")
+    parser.add_argument("--input", required=True, help="The input GenBank file name.")
+    parser.add_argument("--output", required=False, help="The output FASTA file name.")
+    args = parser.parse_args()
+    INPUT_GB_FILE = f"input/{args.input}.gb"
+    if args.output is None:
+        OUTPUT_FAS_FILE = f"output/output_{args.input}.fas"
+    else:
+        OUTPUT_FAS_FILE = f"output/{args.output}.fas"
+    main(INPUT_GB_FILE, OUTPUT_FAS_FILE)
